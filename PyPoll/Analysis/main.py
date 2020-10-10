0e49283923bfadfceb611c7c750899e36c4fa170
jennifer-powell <jennyruepowell@gmail.com>
2020-10-10 15:56:03 -0500

    start of PyPoll

--- PyPoll/Analysis/main.py
+++ PyPoll/Analysis/main.py
@@ -2,21 +2,24 @@ import os
 
 import csv
 
-csvpath = os.path.join('..', 'Resources', 'budget_data.csv')
+csvpath = os.path.join('..', 'Resources', 'election_data.csv')
 
 with open(csvpath) as csvfile:
 
     csvreader = csv.reader(csvfile, delimiter=',')
 
-    print(csvreader)
+    #print(csvreader)
 
     csv_header = next(csvreader)
-    print(f"CSV Header: {csv_header}")
-
+    #print(f"CSV Header: {csv_header}")
+    vote_count= 0
     for row in csvreader:
-        print(row)
+        #print(row)
 
 # * The total number of votes cast
+    
+            vote_count +=1
+    print(f"Total Votes:  {vote_count}")
 # * A complete list of candidates who received votes
 # * The percentage of votes each candidate won
 # * The total number of votes each candidate won
