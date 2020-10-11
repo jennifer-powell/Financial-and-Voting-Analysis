c860b06a0768bffd2ddb2cd4baf2eda63246aacc
jennifer-powell <jennyruepowell@gmail.com>
2020-10-10 22:34:46 -0500

    so far

--- PyBank/Analysis/main.py
+++ PyBank/Analysis/main.py
@@ -2,7 +2,7 @@ import os
 
 import csv
 
-csvpath = os.path.join('..', 'Resources', 'budget_data.csv')
+csvpath = "C:\\Users\\jenzy\\Desktop\\Python--Challenge\\PyBank\\Resources\\budget_data.csv"
 
 with open(csvpath) as csvfile:
 
@@ -14,31 +14,38 @@ with open(csvpath) as csvfile:
     #print(f"CSV Header: {csv_header}")
     
     month_count= 0
-    profit_loss= [1]
+    #Check out your line 18
+    profit_loss= []
     total_profit_loss= 0.0
     for row in csvreader:
-    #    print(row)
+
+        #print(row)
 
 # *The total number of months included in the dataset
             month_count +=1
-    print(f"Total Months:  {month_count}")
- 
     
-# # *The net total amount of "Profit/Losses" over the entire period
-profit_loss= float(row[1])
-total_profit_loss+= profit_loss
-    
-        
-print(f"Total: ${profit_loss}")
-## *The average of the changes in "Profit/Losses" over the entire period
-#average_change.append(profit_loss[i+1] - profit_loss[i])
-# for i in range(len(profit_loss)-1):
-#         change.append(profit_loss[i+1]-profit_loss[i])
+# # *The net total amount of "Profit/Losses" over the entire period, 
 
+            profit_loss= float(row[1])
+            total_profit_loss = total_profit_loss + profit_loss
+    print(f"Total Months:{month_count}")      
+    print(f"Total: ${profit_loss}")
+
+# #The average of the changes in "Profit/Losses" over the entire period
+    
+    
+    finances= []
+    average_change= []
+    for i in range(len(finances)):
+        average_change.append(finances[i+1]-finances[i])
+
+
+    print(finances)
 
 
 
 # *The greatest increase in profits (date and amount) over the entire period
 
-
+#greatest_inc_profits
 # *The greatest decrease in losses (date and amount) over the entire period
+#greatest_dec_losses
--- PyPoll/Analysis/main.py
+++ PyPoll/Analysis/main.py
@@ -13,15 +13,33 @@ with open(csvpath) as csvfile:
     csv_header = next(csvreader)
     #print(f"CSV Header: {csv_header}")
     vote_count= 0
+    
     for row in csvreader:
         #print(row)
 
 # * The total number of votes cast
     
             vote_count +=1
-    print(f"Total Votes:  {vote_count}")
+    # print(f"Total Votes:  {vote_count}")
+
 # * A complete list of candidates who received votes
-# * The percentage of votes each candidate won
+    
+    candidate_list.append(row[2])
+    for row in data if row[2] not in list_of_candidates]
+    
+    
+
+
+
+
 # * The total number of votes each candidate won
+    #total number of times a canidate was mentioned
+# * The percentage of votes each candidate won
+    #number of votes for each canidate/ "votecount"
+
+
+
 # * The winner of the election based on popular vote.
-# * As an example, your analysis should look similar to the one below:
+    #
+
+ #print(candidate:, candidate %, (candidate's total vote count))
