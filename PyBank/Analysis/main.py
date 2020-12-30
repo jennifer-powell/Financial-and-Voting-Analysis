767089a9ca55efd8e3e831080c03ab60a55bd739
jennifer-powell <jennyruepowell@gmail.com>
2020-12-29 19:56:10 -0600

    Finale

--- PyBank/Analysis/main.py
+++ PyBank/Analysis/main.py
@@ -8,6 +8,9 @@ month_count= 0
 month_changepls= []
 month_of_change =[]
 total_profit_loss= 0
+greatest_inc_profits=['', 0]
+greatest_dec_profits= ['', 9999999999999999999]
+
 with open(csvpath) as csvfile:
 
     csvreader = csv.reader(csvfile, delimiter=',')
@@ -17,14 +20,11 @@ with open(csvpath) as csvfile:
     csv_header = next(csvreader)
     #print(f"CSV Header: {csv_header}")
     
-    
-    #current_monthpl= 0
     first_row= next(csvreader)
     last_monthpl= int(first_row[1])
    
     month_changepl= 0
     month_count +=1
-    # profit_loss= []
     total_profit_loss += int(first_row[1])
     
     
@@ -39,34 +39,36 @@ with open(csvpath) as csvfile:
 
         total_profit_loss += int(row[1])
            
-   
-
 # #The average of the changes in "Profit/Losses" over the entire period
     # finding the month to month changes then averaging them
-   # monthly_change
+
         month_changepl=int(row[1]) - last_monthpl
         last_monthpl = int(row[1])
         month_changepls += [month_changepl]
         month_of_change += [row[0]]
-        # month_changepls.append(month_changepl)
+        
 
   
-    sum_changes= sum(month_changepls)
-    average_change= (sum_changes/len(month_changepls))
+        sum_changes= sum(month_changepls)
+        average_change= (sum_changes/len(month_changepls))
     
 
-
-#total_change = total_change + profit_loss
 # *The greatest increase in profits (date and amount) over the entire period
 
-#greatest_inc_profits
+        if month_changepl > greatest_inc_profits[1]:
+            greatest_inc_profits[0] = row[0]
+            greatest_inc_profits[1] = month_changepl
 # *The greatest decrease in losses (date and amount) over the entire period
-#greatest_dec_losses
 
-
-
-    print(f"Total Months:{month_count}")      
+        if month_changepl < greatest_dec_profits[1]:
+            greatest_dec_profits[0] = row[0]
+            greatest_dec_profits[1] = month_changepl
+    print(f"Financial Analysis")
+    print("----------------------------")
+    print(f"Total Months: {month_count}")      
     print(f"Total Profit/Loss: ${total_profit_loss}")
-    print(f"Average Change:{average_change}")  
-    # print(f"Greatest Increase in Profits:{}")  
-    # print(f"Greatest Decrease in Profits:{}")  
+    print(f"Average Change: ${round(average_change, 2)}")  
+    print(f"Greatest Increase in Profits: {greatest_inc_profits[0]}, ${greatest_inc_profits[1]}")  
+    print(f"Greatest Decrease in Profits: {greatest_dec_profits[0]}, ${greatest_dec_profits[1]}")  
+
+    PyBankFile= open("PyBanktext.txt", "a" )
--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # Python--Challenge
+
+These are the results for the PyBank Analysis 
+
+
+Financial Analysis
+----------------------------
+Total Months: 86
+Total Profit/Loss: $38382578
+Average Change: $-2315.12
+Greatest Increase in Profits: 2/1/2012, $1926159
+Greatest Decrease in Profits: 9/1/2013, $-2196167
+
+
+
+These are the results for the PyPoll Analysis:
+
+Election Results
+---------------------
+Total Votes: 3521001
+---------------------
+Li: 14.0% (492940)
+O'Tooley: 3.0% (105630)
+Correy: 20.0% (704200)
+Khan: 63.0% (2218231)
+---------------------
+winner: Khan
