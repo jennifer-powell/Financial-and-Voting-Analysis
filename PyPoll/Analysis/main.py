e4a5b31aca9b10e7cd906708bb492688312579fe
jennifer-powell <jennyruepowell@gmail.com>
2021-01-03 11:05:49 -0600

    Working Code With Screen Shots

--- PyPoll/Analysis/main.py
+++ PyPoll/Analysis/main.py
@@ -33,40 +33,15 @@ with open(csvpath) as csvfile:
         candidates.append(candidate)
         votes= candidate_votes.count(candidate)
         Number_votes.append(votes)
-        # percent= (votes/vote_count)*100
-        # vote_percent.append(percent)
+       
         percent= (votes/vote_count)*100
     winning= max(Number_votes)
     winner= candidates[Number_votes.index(winning)]
-    
-    #         if candidate_votes in candidate:
-    #             candidate.append(candidate_votes)
-    #             set_candidate_votes= set(candidate_votes)
-    #             Number_votes= len(set_candidate_votes)
-    
-    # print(candidate)
-            # if candidate in Number_votes:
-            #     Number_votes[candidate]= Number_votes[candidate] +1
-            # else:
-            #     Number_votes[candidate]=1
-
-            # print(Number_votes[candidate])
-
-
-
-
-# * The total number of votes each candidate won
-    #total number of times a canidate was mentioned
-# * The percentage of votes each candidate won
-    #number of votes for each canidate/ "votecount"
-
-
-
+      
+# * The total number of votes each candidate won    #total number of times a canidate was mentioned
+# * The percentage of votes each candidate won    #number of votes for each canidate/ "votecount"
 # * The winner of the election based on popular vote.
-    #
-
- #print(candidate:, candidate %, (candidate's total vote count))
-
+    
 print(f"Election Results")
 print("---------------------")
 print(f"Total Votes: {vote_count}") 
@@ -75,11 +50,18 @@ print(f"{candidates[0]}: {round((Number_votes[0]/vote_count)*100, 3)}% ({Number_
 print(f"{candidates[1]}: {round((Number_votes[1]/vote_count)*100, 3)}% ({Number_votes[1]})")
 print(f"{candidates[2]}: {round((Number_votes[2]/vote_count)*100, 3)}% ({Number_votes[2]})")
 print(f"{candidates[3]}: {round((Number_votes[3]/vote_count)*100, 3)}% ({Number_votes[3]})")
-
 print("---------------------")
 print(f"winner: {winner}")
                  
 
 
 
-PyPollFile= open("PyPolltext.txt", "a" )
+PyPollFile= open("PyPolltext.txt", "w" )
+PyPollFile.write(str(vote_count))
+PyPollFile.write(str(candidate_votes))
+PyPollFile.write(str(Number_votes))
+PyPollFile.write(str(vote_percent))
+PyPollFile.write(str(votes))
+PyPollFile.write(str(percent))
+PyPollFile.write(str(winning))
+PyPollFile.write(str(winner))
